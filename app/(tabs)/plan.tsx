2a4973d7938ba9056b4f262c89bde37c075e054c
zjeee <wiritipon.t@ku.th>
2025-01-21 03:10:28 +0700

    feat(tabs): add tabs button navigate  using expo router

---
+++ app/(tabs)/plan.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text,Button, View, XGroup, XStack, YStack } from "tamagui";
+
+export default function plan (){
+  return (
+      <View>
+           <Text>
+                       add
+                   </Text>
+       </View>
+  )
+}
---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text,Button, View, XGroup, XStack, YStack } from "tamagui";
+
+export default function profile(){
+  return (
+    <View>
+        <Text>
+                    aพdd
+                </Text>
+    </View>
+  )
+}
