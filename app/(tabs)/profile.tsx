8a0f4bc8fb821efa5734ad34fe91b85dd7472aea
Chayathon.R <162368012+ThirdChyr@users.noreply.github.com>
2025-02-03 01:01:11 +0700

    List: 1.Create Middle of box

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,18 +1,24 @@
 import React from 'react'
-
+import { useState } from 'react'
 import { Text, Button, View, XGroup, XStack, YStack, ScrollView, styled } from "tamagui";
-import PublicPlanBox from '@/components/PublicPlanBox';
+
 import { ThemedView } from '@/components/ThemedView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedLogo } from '@/components/ThemedLogo';
 import SvgComponent from '@/components/Bgelement';
+    
 
+const [username, setUsername] = useState('');
+    
 export default function profile() {
   return (
     <ThemedView>
       <Backround>
         <SvgComponent />
       </Backround>
+      <Foreground>
+        
+      </Foreground> 
   </ThemedView>
   )
 }
@@ -24,6 +30,23 @@ const Backround = styled(View, {
   width: '100%',
   height: '100%',
   zIndex: -1,  
+})
+const Foreground = styled(View,{
+  width: '90%',               
+  height: 600,                
+  backgroundColor: 'white',   
+  borderRadius: 20,           
+  opacity: 1,                 
+  zIndex: 1,                 
+  position: 'absolute',       
+  top: '50%',                 
+  left: '50%',               
+  transform: [{ translateX: '-50%' }, { translateY: '30%' }],  
+  flex: 1,                    
+  justifyContent: 'center',  
+  alignItems: 'center',       
+
+
 })
 
 
