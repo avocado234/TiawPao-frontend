db6b30d84397b1ba44b5fbd5d420344c9d73071f
Chayathon.R <162368012+ThirdChyr@users.noreply.github.com>
2025-02-02 22:26:03 +0700

    List: 1.Adding Background with styled() syntax

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
-import { Text,Button, View, XGroup, XStack, YStack, ScrollView } from "tamagui";
-// import ColorScrollList from '@/components/ColorScrollList';
-export default function profile(){
+
+import { Text, Button, View, XGroup, XStack, YStack, ScrollView, styled } from "tamagui";
+import PublicPlanBox from '@/components/PublicPlanBox';
+import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedLogo } from '@/components/ThemedLogo';
+import SvgComponent from '@/components/Bgelement';
+
+export default function profile() {
   return (
-    <ScrollView>
-       {/* <ColorScrollList color="#32a852"/> */}
-    </ScrollView>
+    <ThemedView>
+      <Backround>
+        <SvgComponent />
+      </Backround>
+  </ThemedView>
   )
 }
+
+const Backround = styled(View, {
+  position: 'absolute', 
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100%',
+  zIndex: -1,  
+})
+
+
+
