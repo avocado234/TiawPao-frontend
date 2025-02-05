1dee25bbc3ca97ad81a4995bb5a9f955db8bf5a2
Chayathon.R <162368012+ThirdChyr@users.noreply.github.com>
2025-02-06 00:25:16 +0700

    List: Version 1 i Cannot Merge because, I just dowload some package T_T

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,52 @@
 import React from 'react'
-import { Text,Button, View, XGroup, XStack, YStack, ScrollView } from "tamagui";
-export default function profile(){
-  return (
-    <ScrollView>
+import { Text, Button, View, XGroup, XStack, YStack, ScrollView, styled } from "tamagui";
+import Propage from '@/components/Profile_page';
+import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedLogo } from '@/components/ThemedLogo';
+import SvgComponent from '@/components/Bgelement';
     
-    </ScrollView>
+
+
+    
+export default function profile() {
+  return (
+    <ThemedView>
+      <Backround>
+        <SvgComponent />
+      </Backround>
+      {/* <Foreground> */}
+       <Propage/>
+      {/* </Foreground>  */}
+  </ThemedView>
   )
 }
+
+const Backround = styled(View, {
+  position: 'absolute', 
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100%',
+  zIndex: -1,  
+})
+// const Foreground = styled(View,{
+//   width: '90%',               
+//   height: 650,                
+//   backgroundColor: 'white',   
+//   borderRadius: 20,           
+//   opacity: 1,                 
+//   zIndex: 1,                 
+//   position: 'absolute',       
+//   top: '50%',                 
+//   left: '50%',               
+//   transform: [{ translateX: '-50%' }, { translateY: '20%' }],  
+//   flex: 1,                    
+//   justifyContent: 'center',  
+//   alignItems: 'center',       
+
+
+// })
+
+
+
