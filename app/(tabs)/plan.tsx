a4d73c3eb26d8c1a6eccd834ddbbb35557690444
Thitsanu <Thitsanu.w@ku.th>
2025-02-18 22:19:31 +0700

    fix : edit layout myplan

--- app/(tabs)/plan.tsx
+++ app/(tabs)/plan.tsx
@@ -5,11 +5,13 @@ import {
   View,
   Image,
   TouchableOpacity,
+  ScrollView,
 } from 'react-native';
 import MyPlanBox from '@/components/MyplanBox';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import Bgelement from '@/components/Bgelement';
 
 const plan: React.FC = () => {
   const [isEditMode, setIsEditMode] = useState(false);
@@ -17,17 +19,12 @@ const plan: React.FC = () => {
   return (
     <SafeAreaView style={styles.container}>
       <ThemedView style={styles.themedView}>
-        <Image
-          className="absolute -top-96"
-          source={require("@/assets/images/Bgcycle.png")}
-          style={styles.backgroundImage}
-        />
+        <Bgelement/>
         <View style={styles.headerWrapper}>
-          <ThemedText className=' inset-6' style={styles.headerText}>My Plan</ThemedText>
-        </View>
-
+          <ThemedText className='px-5' style={styles.headerText}>My Plan</ThemedText>
+        <ScrollView showsVerticalScrollIndicator={false} >
         <MyPlanBox isEditMode={isEditMode} />
-
+        </ScrollView>
         {/* ปุ่ม Edit/Check */}
         <TouchableOpacity
           style={styles.editButton}
@@ -36,37 +33,29 @@ const plan: React.FC = () => {
           
           <MaterialCommunityIcons name={isEditMode ? "file-check-outline" : "pencil"} size={32} color="#fff" />
         </TouchableOpacity>
-
+        </View>
       </ThemedView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1 },
+  container: { flex: 1 , paddingVertical: 60},
   themedView: { flex: 1 },
-  backgroundImage: {
-    position: 'absolute',
-    top: -185,
-    right: -180,
-    width: '200%',
-    height: '90%',
-  },
   headerWrapper: {
-    marginTop: 20,
-    paddingHorizontal: 50,
+    marginTop: 10,
   },
   headerText: {
     fontSize: 44,
     color: 'white',
     fontWeight: 'bold',
-    marginBottom: 30,
+    marginBottom: 10,
   },
   editButton: {
     position: "absolute",
     backgroundColor: "#5680EC",
     end : "5%",
-    top : "83%",
+    top : "80%",
     padding: 16,
     borderRadius: 30,
   },
