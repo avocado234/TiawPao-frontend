0c53d4fda79b643d9a1ea518392c8fac00bb486b
Chayathon.R <162368012+ThirdChyr@users.noreply.github.com>
2025-02-20 01:56:34 +0700

    Create TripGenAi Perfect

--- app/tripGenAi.tsx
+++ app/tripGenAi.tsx
@@ -1,22 +1,38 @@
 import React from 'react';
-import { useRouter,router } from 'expo-router'; 
+import { useRouter,router,useLocalSearchParams } from 'expo-router'; 
 
 import { View, Text, SafeAreaView, Pressable, useColorScheme ,Image} from 'react-native';
 import { ArrowLeft ,ArrowRightToLine} from "@tamagui/lucide-icons";
 import { Button, XStack, YStack } from 'tamagui';
 import { ImageBackground } from 'expo-image';
 
-const Themecolor = () => {
-  const theme = useColorScheme();
-  let opacitySize = 0.0;
-  if (theme === 'dark') {
-    opacitySize = 0.6;
-  } else {
-    opacitySize = 0.4;
-  }
-  return { opacitySize };
+// const Themecolor = () => {
+//   const theme = useColorScheme();
+//   let opacitySize = 0.0;
+//   if (theme === 'dark') {
+//     opacitySize = 0.6;
+//   } else {
+//     opacitySize = 0.4;
+//   }
+//   return { opacitySize };
+// };
+const Goto_GenProbs = () =>{
+const param = useLocalSearchParams();
+const {tripName,region,province,startDate,startTime,endDate,endTime,visibility} = param;
+  router.push({
+    pathname: "/genaiselected",
+    params: {
+      tripName: tripName,
+      region: region,
+      province: province,
+      startDate: startDate,
+      startTime: startTime,
+      endDate: endDate,
+      endTime: endTime,
+      visibility:visibility
+    }
+  });
 };
-
 const Return_Button = () => {
  
   const BacktoAdd = () => {
@@ -33,14 +49,19 @@ const Return_Button = () => {
 };
 
 const MainText = () => {
-  const trip = "ChonBuri";
+const param = useLocalSearchParams();
+let {tripName,region,province,startDate,startTime,endDate,endTime,visibility} = param;
+if(province === "")
+{
+  province ="Random?" 
+}
   return (
    <View>
        <Text style={styles.MainFont}>Gennarate your trip</Text>
        <XStack>
         <XStack>
           <Text style={styles.MainFont} >in </Text>
-          <Text style={[styles.ProvinceFont,{ fontWeight:"bold"}]}>{trip}</Text>
+          <Text style={[styles.ProvinceFont,{ fontWeight:"bold"}]}>{province}</Text>
           </XStack>
        <Image
         source={require("@/assets/images/Flag-thai.png")}
@@ -54,16 +75,65 @@ const MainText = () => {
 
 const Main = () => {
   let backgroundColor;
-  
+  let backGroundPic ;
   const theme = useColorScheme();
-  if (theme === 'dark') {
-   
-  } else {
-    
-  }
+  const param = useLocalSearchParams();
+  const {tripName,region,province,startDate,startTime,endDate,endTime,visibility} = param;
+  if(region === "central")
+    {
+      backGroundPic =require(`@/assets/images/region/central.jpeg`);
+    }
+    else if(region === "northern")
+    {
+      backGroundPic =require(`@/assets/images/region/northern.jpg`);
+    }
+    else if(region === "northeastern")
+    {
+      backGroundPic =require(`@/assets/images/region/northeastern.jpg`);
+    }
+    else if(region === "eastern")
+    {
+      backGroundPic =require(`@/assets/images/region/eastern.jpg`);
+    }
+    else if(region === "western")
+    {
+      backGroundPic =require(`@/assets/images/region/western.jpg`);
+    }
+    else if(region === "southern")
+    {
+      backGroundPic =require(`@/assets/images/region/southern.jpg`);
+    }
+    else
+    {
+      let random = Math.floor(Math.random() * (5 - 0 + 1)) + 0;
+      if(random === 0)
+      {
+        backGroundPic =require(`@/assets/images/region/central.jpeg`);
+      }
+      else if(random === 1)
+      {
+        backGroundPic =require(`@/assets/images/region/northern.jpg`);
+      }
+      else if(random === 2)
+      {
+        backGroundPic =require(`@/assets/images/region/northeastern.jpg`);
+      }
+      else if(random === 3)
+      {
+        backGroundPic =require(`@/assets/images/region/eastern.jpg`);
+      }
+      else if(random === 4)
+      {
+        backGroundPic =require(`@/assets/images/region/western.jpg`);
+      }
+      else
+      {
+        backGroundPic =require(`@/assets/images/region/southern.jpg`);
+      }
+    }
   return (
     <ImageBackground
-      source={require("@/assets/images/koh-larn-thailand.jpg")}
+      source={backGroundPic}
       style={styles.ImageBackGroud}>
       <SafeAreaView style={styles.ThemeSafeArea}>
         <XStack style={styles.XStackReturnButton}>
@@ -76,12 +146,13 @@ const Main = () => {
             <Text style={styles.DescribeFont}>generate a personalized trip</Text>
           </View>
           <View style={{marginTop:37}}>
-            <Button style={styles.ButtonOnlyOne}>
-              <Text style={styles.DescribeFont}>Start</Text>
-              <ArrowRightToLine size={20} color={"black"} />
+            <Button style={styles.ButtonOnlyOne}
+            onPress={Goto_GenProbs}
+            >
+              <Text style={[styles.DescribeFont,{color:'white'}]}>Start</Text>
+              <ArrowRightToLine size={20} color={"white"} />
             </Button>
           </View>
-          
         </YStack>
       </SafeAreaView>
     </ImageBackground>
@@ -102,12 +173,12 @@ const styles = {
     paddingHorizontal: 16,
     paddingVertical: 8,
     alignItems: 'center',
-    backgroundColor: 'red',
+    //backgroundColor: 'red',
   
   },
   YStackMainComponent: {
     marginTop: 50,
-    backgroundColor: 'red',
+   // backgroundColor: 'red',
     width: '100%',
     height: 500,
     alignItems: 'left',
@@ -117,7 +188,7 @@ const styles = {
 
   },
   ButtonOnlyOne:{
-    
+    backgroundColor:"#203B82",
     borderRadius:50,
     width:100,
     heigth:38,
