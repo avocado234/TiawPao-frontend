46dfc4a269c3ce082cb98426b834582fe51fed96
Thitsanu <Thitsanu.w@ku.th>
2025-02-22 12:08:19 +0700

    fix : add data

--- app/(tabs)/plan.tsx
+++ app/(tabs)/plan.tsx
@@ -13,10 +13,26 @@ import { ThemedView } from '@/components/ThemedView';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import Bgelement from '@/components/Bgelement';
 
-const { width, height } = Dimensions.get('window');
+const initialTrips = [
+  { id: 1, nametrip: "Chonburi trip", price: "500$", date: "24 Jan - 26 Jan", location: "Pattaya, Chonburi" },
+  { id: 2, nametrip: "Chiang Mai Tour", price: "600$", date: "13 June - 19 June", location: "Chiang Mai" },
+  { id: 3, nametrip: "Go Khon Kaen", price: "700$", date: "13 June - 19 June", location: "Khon Kaen" },
+  { id: 4, nametrip: "Rayong First Time", price: "700$", date: "24 Jan - 25 Jan", location: "Rayong" },
+  { id: 5, nametrip: "Rayong First Time", price: "700$", date: "24 Jan - 25 Jan", location: "Rayong" },
+  { id: 6, nametrip: "Rayong First Time", price: "700$", date: "24 Jan - 25 Jan", location: "Rayong" },
+  { id: 7, nametrip: "Rayong First Time", price: "700$", date: "24 Jan - 25 Jan", location: "Rayong" },
+  { id: 8, nametrip: "Rayong First Time 12", price: "700$", date: "24 Jan - 25 Jan", location: "Rayong" },
+];
+
+const { width, height } = Dimensions.get('screen');
 
 const Plan: React.FC = () => {
   const [isEditMode, setIsEditMode] = useState<boolean>(false);
+  const [trips, setTrips] = useState(initialTrips);
+
+  const handleDelete = (id: number) => {
+    setTrips(trips.filter(trip => trip.id !== id));
+  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -28,7 +44,7 @@ const Plan: React.FC = () => {
         <ScrollView 
           showsVerticalScrollIndicator={false} 
           contentContainerStyle={styles.scrollContainer}>
-          <MyPlanBox isEditMode={isEditMode} />
+          <MyPlanBox trips={trips} isEditMode={isEditMode} onDelete={handleDelete} />
         </ScrollView>
         {/* Edit Button */}
         <TouchableOpacity
@@ -51,14 +67,13 @@ const styles = StyleSheet.create({
   themedView: { flex: 1 },
   headerWrapper: {
     marginTop: height * 0.05, 
-  
   },
   headerText: {
     fontSize: width * 0.1,
     marginHorizontal: 20, 
     color: 'white',
     fontWeight: 'bold',
-    marginBottom: height * 0.02,
+    marginBottom: height * 0.01,
   },
   scrollContainer: {
     flexGrow: 1,
