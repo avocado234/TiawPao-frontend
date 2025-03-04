e6c340079871e88c7d146bd96f55bc345f1b49c6
Thitsanu <Thitsanu.w@ku.th>
2025-03-04 22:19:28 +0700

    edit Language in axiosTATInstance.ts

--- utils/axiosTATInstance.ts
+++ utils/axiosTATInstance.ts
@@ -4,7 +4,7 @@ const apiTAT = axios.create({
     baseURL:'https://tatdataapi.io/api/v2',
     headers: {
       'accept': '*/*',
-      'Accept-Language': 'th',
+      'Accept-Language': 'en',
       'x-api-key': '80JkXNR5DiYWslw5NoR4gGVatS3GKw9G'
     }
   });
