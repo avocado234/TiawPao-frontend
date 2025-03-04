a081b169dec2692aa89fb62496f4a007cad1e6a5
zjeeee <wiritipon.t@ku.th>
2025-03-05 01:11:47 +0700

    fix:add onpress and route to tripmanually

--- app/(tabs)/search/index.tsx
+++ app/(tabs)/search/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -6,6 +6,7 @@ import {
   TouchableOpacity,
   Dimensions,
   ScrollView,
+  ActivityIndicator,
 } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
@@ -20,8 +21,31 @@ import Filterplan from '@/components/Filterplan';
 import TripCard from '@/components/TripCard';
 import { useRouter } from "expo-router";
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import api from '@/utils/axiosInstance';
+import LoadingComponent from '@/components/LoadingComponent';
+
 const { width, height } = Dimensions.get('window');
 const FILTER_PANEL_HEIGHT = height * 0.25;
+
+interface PlanData {
+  author_email: string;
+  author_img: string;
+  end_date: string;
+  end_time: string;
+  plan_id: string;
+  province_id: string;
+  province_label: string;
+  region_label: string;
+  start_date: string;
+  start_time: string;
+  trip_location: any[];
+  trip_name: string;
+  visibility: boolean;
+}
+
+// interface Trip {
+//   plan_data: PlanData;
+// }
 
 const tripData = [
   { id: 1, user: "Jame Macdonell", nametrip: "Chonburi trip", price: "500 ฿", date: "24 Jan - 26 Jan", rating: "4.8", location: "Chonburi", description: "This plan is very good plan in Pattaya" },
@@ -31,10 +55,29 @@ const tripData = [
 ];
 
 const Search: React.FC = () => {
-  const router = useRouter();
+
+  const [planDataArray, setPlanDataArray] = useState<PlanData[]>([]);
   const [filterOpen, setFilterOpen] = useState<boolean>(false);
   const [filteredTrips, setFilteredTrips] = useState(tripData);
+  const [loading, setLoading] = useState<boolean>(true);
   const filterHeight = useSharedValue(0);
+
+  const getPublicPlan = async () => {
+    try {
+      const response = await api.get('/plan/getpublicplan');
+      setPlanDataArray(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getPublicPlan();
+  }, []);
+
+  const router = useRouter();
 
   const animatedFilterStyle = useAnimatedStyle(() => ({ height: filterHeight.value }));
 
@@ -42,27 +85,56 @@ const Search: React.FC = () => {
     filterHeight.value = withTiming(filterOpen ? 0 : FILTER_PANEL_HEIGHT, { duration: 500 });
     setFilterOpen(!filterOpen);
   };
-
+ 
+  if (loading) {
+    return (
+     <LoadingComponent/>
+    );
+  }
   return (
-    <SafeAreaView className=' mt-5' style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <ThemedView style={styles.themedView}>
         <Bgelement />
         <View style={styles.headerWrapper}>
-          <ThemedText  style={styles.headerText}>Public Plan</ThemedText>
+          <ThemedText style={styles.headerText}>Public Plan</ThemedText>
           <TouchableOpacity onPress={toggleFilter}>
-            <MaterialIcons style={{ opacity:0.75 }} name={filterOpen  ? 'filter-list-off' : 'filter-list'} size={36} color= "#fff" />
+            <MaterialIcons style={{ opacity: 0.75 }} name={filterOpen ? 'filter-list-off' : 'filter-list'} size={36} color="#fff" />
           </TouchableOpacity>
         </View>
         <Animated.View style={[styles.filterContainer, animatedFilterStyle]}>
           <Filterplan trips={tripData} setFilteredTrips={setFilteredTrips} />
         </Animated.View>
-        <ScrollView style={styles.scrollView} contentContainerStyle={styles.scrollContainer} showsVerticalScrollIndicator={false}>
-          {filteredTrips.map((trip) => (
-            <TouchableOpacity key={trip.id} activeOpacity={0.8} style={{ alignItems: "center"}} >
-              <TripCard trip={trip}  />
-            </TouchableOpacity>
-          ))}
-        </ScrollView>
+        {loading ? (
+          <View style={styles.loadingContainer}>
+            <ActivityIndicator size="large" color="#fff" />
+          </View>
+        ) : (
+          <ScrollView
+  style={styles.scrollView}
+  contentContainerStyle={styles.scrollContainer}
+  showsVerticalScrollIndicator={false}
+>
+{planDataArray.map((trip, index) => (
+  <TouchableOpacity
+    key={index}
+    activeOpacity={0.8}
+    style={{ alignItems: "center" }}
+    onPress={() => {
+      router.replace({
+        pathname: "/(tabs)/add/tripmanually",
+        params: {
+          planID: trip.plan_id,
+        }
+      });
+    }}
+  >
+    <TripCard {...trip} />
+  </TouchableOpacity>
+))}
+
+</ScrollView>
+
+        )}
       </ThemedView>
     </SafeAreaView>
   );
@@ -94,6 +166,11 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     minHeight: height,
     paddingBottom: height * 0.08,
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
