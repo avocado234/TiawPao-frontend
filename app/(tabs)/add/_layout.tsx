eb24fb47eb17576536da1a26afb53511a307b1fb
zjeeee <wiritipon.t@ku.th>
2025-03-07 01:50:47 +0700

    fix:tripmaual

--- app/(tabs)/add/_layout.tsx
+++ app/(tabs)/add/_layout.tsx
@@ -10,6 +10,7 @@ export default function AddLayout() {
 
     <Stack screenOptions={{ gestureEnabled: false }}>
           <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="createtrip" options={{ headerShown: false }} />
           <Stack.Screen name="tripmanually" options={{ headerShown: false }} />
           <Stack.Screen name="genaiselected" options={{headerShown: false}} />
           <Stack.Screen name="tripgenai" options={{headerShown: false}} />
