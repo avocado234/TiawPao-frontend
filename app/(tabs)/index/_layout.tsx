e0f4356c8e2b123acbcd6f625a3a7ac1d89edb43
Thitsanu <Thitsanu.w@ku.th>
2025-03-08 01:10:02 +0700

    fix: set initial route name for the Stack navigation

--- app/(tabs)/index/_layout.tsx
+++ app/(tabs)/index/_layout.tsx
@@ -8,10 +8,12 @@ export default function IndexLayout() {
 
   return (
 
-    <Stack screenOptions={{ gestureEnabled: false }}>
+    <Stack screenOptions={{ gestureEnabled: false }} initialRouteName="home">
+          <Stack.Screen name="home" options={{headerShown: false}} />
           <Stack.Screen name="index" options={{ headerShown: false }} />
           <Stack.Screen name="HomeRecommend" options={{headerShown : false}}/>
           <Stack.Screen name="homedetail" options={{headerShown: false}} />
+        
      </Stack>
   );
 }
