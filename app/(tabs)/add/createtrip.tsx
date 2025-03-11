028628888f58f3af1c1842ff7fb10b23c3a8dfa3
Chayathon.R <162368012+ThirdChyr@users.noreply.github.com>
2025-03-11 23:26:18 +0700

    feat(createtrip): add description field to trip creation

--- app/(tabs)/add/createtrip.tsx
+++ app/(tabs)/add/createtrip.tsx
@@ -314,6 +314,7 @@ export default function CreateTrip() {
         startTime: startTime.toISOString(),
         endDate: endDate.toISOString(),
         endTime: endTime.toISOString(),
+        description: description,
         visibility: selectedOption
       }
     });
